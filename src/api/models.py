efed1135c34f59b82da99fe7353d654991063cfd
rafalebre <rafaellebre100@gmail.com>
2023-06-06 17:15:41 +0000

    Novas tabelas criadas e adicionadas ao admin

--- src/api/models.py
+++ src/api/models.py
@@ -36,3 +36,52 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    estimated_value = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String(120))
+
+    user = db.relationship('User', backref='products')
+
+class Service(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    estimated_value = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String(120))
+
+    user = db.relationship('User', backref='services')
+
+class Trade(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    offering_user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    receiving_user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
+    offered_product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    offered_service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
+    status = db.Column(db.String(30), nullable=False)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
+    content = db.Column(db.Text, nullable=False)
+
+class Wishlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
+
